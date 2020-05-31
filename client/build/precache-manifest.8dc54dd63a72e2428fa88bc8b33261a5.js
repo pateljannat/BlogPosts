2cde294ec27ca1deea5eb80a06e7ce86032ed6ba
paperplane <paperplane@test-spare.local>
2020-05-31 12:46:42 +0530

    Build created

---
+++ client/build/precache-manifest.8dc54dd63a72e2428fa88bc8b33261a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d41ac5ced01b5afe3af2716ba11988",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efd8090e16254ffc8625",
+    "url": "/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "56c21b5c382a64ea3c73",
+    "url": "/static/css/main.e7eb6947.chunk.css"
+  },
+  {
+    "revision": "efd8090e16254ffc8625",
+    "url": "/static/js/2.16c7fcb4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.16c7fcb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56c21b5c382a64ea3c73",
+    "url": "/static/js/main.da662ece.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
